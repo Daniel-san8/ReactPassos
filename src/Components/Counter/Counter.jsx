df3b0e23c9df963d9a2a32d4b7f55d2b2f76007a
Daniel San <danielgdt88@hotmail.com>
2024-06-13 19:46:55 -0300

    setando contador no set state

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -3,17 +3,22 @@ import React from "react";
 class Counter extends React.Component {
   constructor() {
     super();
-    console.log("opa");
+    this.state = { contador: 0 };
+    console.log("construindo classe counter");
   }
 
   UNSAFE_componentWillMount() {
     console.log("o nosso componente contador será montado");
   }
 
+  componentDidMount() {
+    console.log('componente foi montado """check"""');
+  }
+
   render() {
     return (
       <div>
-        <h1>0</h1>
+        <h1>{this.state.contador}</h1>
         <button>diminuir</button>
         <button>aumentar</button>
       </div>
